1b183dbb1114344e2c7c85701c8de9a1f5458bad
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-13 16:04:02 +0800

    Determine the length of the argument
    Determine the length of the argument which the user input, if the user enter two numbers then return the numbers to month and year, if the user just enter a number then return the month, and print the month of the current year, if the user doesn't enter anything, then print the current month and year.

--- TestDisplayCalendars.java
+++ TestDisplayCalendars.java
@@ -12,6 +12,38 @@ public class TestDisplayCalendars {
 		int month = Integer.parseInt(args[0]);
 		int year = Integer.parseInt(args[1]);
 
+
+		//determine the length of the argument then print the calendar which the user want
+		if(args.length == 2){
+			displayCalendar = new DisplayCalendars(year, month, 1);
+		}
+		else if(args.length == 1){
+			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), month, 1);
+		}
+		else{
+			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), gregoriancalendar.get(Calendar.MONTH) + 1, 1);
+		}
+
+
+
+
+
+
+		//judge the input
+//		if(args.length == 2){
+//			displayCalendar = new DisplayCalendars(Integer.valueOf(args[1]), Integer.valueOf(args[0]), 1);
+//		}
+//		else if(args.length == 1){
+//			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), Integer.valueOf(args[0]), 1);
+//		}
+//		else{
+//			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), gregoriancalendar.get(Calendar.MONTH) + 1, 1);
+//		}
+		
+
+
+
+
 		
 	}
 }
