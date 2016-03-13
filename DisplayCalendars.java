aea7aecffce6880a18f95e87d39a8e4c3fb6ef4a
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-13 15:41:46 +0800

    Determine how many days in the month
    Determine how many days in the month which the user enter, if the month is 1, 3, 5, 7, 8, 10, 12 then return 31 days, if the month is 4, 6, 9, 11 then return 30 days.  And then, create a method to determine whether the year is the leap year, if the year is leap year then February return 29 days, if the year isn't the leap year then return 28 days.

--- DisplayCalendars.java
+++ DisplayCalendars.java
@@ -53,4 +53,26 @@ public class DisplayCalendars {
 	    return monthName;
 	}
 
+
+	//get the number of days in the month
+	public int getNumberOfDaysInMonth() {
+	    if (month == 1 || month == 3 || month == 5 || month == 7 ||
+	  	    month == 8 || month == 10 || month == 12){
+	      	return 31;
+	    }
+		else if (month == 4 || month == 6 || month == 9 || month == 11){
+	      	return 30;
+	    }
+		else if (month == 2){
+	    	return isLeapYear() ? 29 : 28;
+	    }
+	    // If month is incorrect
+	    return 0; 
+	  }
+
+
+	  //determine whether the year which the user enter is leap year
+	  public boolean isLeapYear() {
+	    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+	  }
 }
