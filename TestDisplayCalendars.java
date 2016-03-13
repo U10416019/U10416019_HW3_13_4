45629973ab183f0c179b48462ca5a3911224f7b8
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-13 16:15:31 +0800

    Change another way to display the calendar
    I find the error if the user doesn't enter two variables, so I change a new way to determine the length of the argument, and it also can let the user enter the numbers on the same line.

--- TestDisplayCalendars.java
+++ TestDisplayCalendars.java
@@ -8,42 +8,16 @@ public class TestDisplayCalendars {
 		//create data field
 		GregorianCalendar gregoriancalendar = new GregorianCalendar();
 		DisplayCalendars displayCalendar;
-		//create variable month and year which type are int
-		int month = Integer.parseInt(args[0]);
-		int year = Integer.parseInt(args[1]);
-
 
 		//determine the length of the argument then print the calendar which the user want
 		if(args.length == 2){
-			displayCalendar = new DisplayCalendars(year, month, 1);
+			displayCalendar = new DisplayCalendars(Integer.valueOf(args[1]), Integer.valueOf(args[0]), 1);
 		}
 		else if(args.length == 1){
-			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), month, 1);
+			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), Integer.valueOf(args[0]), 1);
 		}
 		else{
 			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), gregoriancalendar.get(Calendar.MONTH) + 1, 1);
-		}
-
-
-
-
-
-
-		//judge the input
-//		if(args.length == 2){
-//			displayCalendar = new DisplayCalendars(Integer.valueOf(args[1]), Integer.valueOf(args[0]), 1);
-//		}
-//		else if(args.length == 1){
-//			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), Integer.valueOf(args[0]), 1);
-//		}
-//		else{
-//			displayCalendar = new DisplayCalendars(gregoriancalendar.get(Calendar.YEAR), gregoriancalendar.get(Calendar.MONTH) + 1, 1);
-//		}
-		
-
-
-
-
-		
+		}	
 	}
 }
