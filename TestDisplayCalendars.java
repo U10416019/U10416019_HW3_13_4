fa89f49fed0242513e59e9f57fb659fe8fda0fec
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-13 15:16:35 +0800

    Data fields and the variables which the user enter
    This file create a variable of GregorianCalendar, and also create two variables month and year which can let the user enter the month and the year in the same line.

---
+++ TestDisplayCalendars.java
@@ -0,0 +1,17 @@
+//U10416019鄭文琪
+
+import java.util.*;
+
+public class TestDisplayCalendars {
+	public static void main(String[] args){
+		
+		//create data field
+		GregorianCalendar gregoriancalendar = new GregorianCalendar();
+		DisplayCalendars displayCalendar;
+		//create variable month and year which type are int
+		int month = Integer.parseInt(args[0]);
+		int year = Integer.parseInt(args[1]);
+
+		
+	}
+}
